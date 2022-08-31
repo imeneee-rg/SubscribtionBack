88046318380778a7afe73d1023ab711098ed2d53
imeneee-rg <benmoussaimen97@gmail.com>
2022-08-31 12:32:20 +0100

    UserModel

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        reuired : true
+    },
+    email : {
+        type :String,
+        required : true,
+        unique : true
+    },
+    
+    password : {
+        type : String ,
+        required : true
+    },
+    avatar : {
+        type : String,
+        default : 'avatar.png'
+    }
+});
+
+
+module.exports = mongoose.model("User", UserSchema);
