54fe15f832f3ffd116eeaa58a9fd4eadedbe1172
imeneee-rg <benmoussaimen97@gmail.com>
2022-08-31 13:18:50 +0100

    Add Wallet model

---
+++ models/Wallet.js
@@ -0,0 +1,25 @@
+{/*const mongoose = require("mongoose");
+
+const WalletSchema = new mongoose.Schema({
+
+
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+
+    solde :{
+    type : Number,
+    required : true,
+    default : 0
+},
+
+
+
+
+
+
+})
+
+module.exports = mongoose.model("Wallet", WalletSchema);*/}
